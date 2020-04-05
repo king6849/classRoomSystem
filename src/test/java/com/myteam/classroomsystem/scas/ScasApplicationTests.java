01ce73b17769bf00222b2e7d5214ab60b342fe3c
king <965853365@qq.com>
2020-04-05 10:44:50 +0800

    空项目

---
+++ src/test/java/com/myteam/classroomsystem/scas/ScasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myteam.classroomsystem.scas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
